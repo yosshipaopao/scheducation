c33d107581e2e5d0d5d396c7953bbe4ad5c376d2
yosshipaopao <yosshipaopao@gmail.com>
2023-08-13 00:32:01 +0900

    多分なんもしてない

--- src/routes/schedule/edit/date/[year]/[month]/[date]/+page.server.ts
+++ src/routes/schedule/edit/date/[year]/[month]/[date]/+page.server.ts
@@ -54,35 +54,45 @@ export const actions = {
         const {year, month, date} = scheduleScript.check("week", params.year ?? defaults.year, params.month ?? defaults.month, params.date ?? defaults.date);
         const baseDate = new Date(year, month - 1, date);
         const baseInt = scheduleScript.convertDate(year, month, date)
-        const defaultSchedule = await db.select({
+        const before = await GetDateSchedule(db, baseDate);
+        const specialSchedule = new Set((await db.select({
             time: schedule.time,
-            subject: schedule.subject,
-            belongings: schedule.belongings,
-            memo: schedule.memo,
-            special: schedule.special
-        }).from(schedule).where(eq(schedule.date, baseDate.getDay())).orderBy(schedule.time);
-        const specialSchedule = await db.select({
-            time: schedule.time,
-            subject: schedule.subject,
-            belongings: schedule.belongings,
-            memo: schedule.memo,
-            special: schedule.special
-        }).from(schedule).where(and(eq(schedule.date, baseInt), ne(schedule.special, 0)));
+        }).from(schedule).where(eq(schedule.date, baseInt))).map(v => v.time));
 
-        const fixed = data.filter((v) =>v.special).find(v=>specialSchedule.find((w)=>(w.time===v.time&&(w.subject!==v.subject||w.belongings!==v.belongings||w.memo!==v.memo))));
-        console.log(fixed)
-        //const isOldIds = special.length == 0 ? new Set<string>() : new Set<string>((await db.select({id: subject.id}).from(subject).where(inArray(subject.id, special.map((v: any) => v.id)))).map(v => v.id as string));
-        //const newSubjects = special.filter((v: any) => !isOldIds.has(v.id)).map((v: any) => ({...v, special: 1}));
+        const add = [];
+        const update = [];
+        const del: number[] = [];
+        for (let i = 0; i < Math.max(before.length, data.length); i++) {
+            if (data[i].subject === "") continue;//無効判定
+            if (!before[i]) add.push(data[i]);//追加(新規)
+            else if (!data[i]) del.push(before[i].time);//削除(削除)
+            else if (!data[i].special) {
+                if (specialSchedule.has(i)) del.push(i);//通常の場合、特殊スケジュールがあれば削除
+            } else if (before[i].subject.id !== data[i].subject || before[i].belongings !== data[i].belongings || before[i].memo !== data[i].memo) {//変更チェック
+                if (specialSchedule.has(i)) update.push(data[i]);//もともと特殊スケジュールがあれば更新
+                else add.push(data[i]);//なければ追加
+            }
+        }
+        const isOldIds = special.length == 0 ? new Set<string>() : new Set<string>((await db.select({id: subject.id}).from(subject).where(inArray(subject.id, special.map((v: any) => v.id)))).map(v => v.id as string));
+        const newSubjects = special.filter((v: any) => !isOldIds.has(v.id)).map((v: any) => ({...v, special: 1}));
 
 
-        //console.log(fixed);
-        /*
-        if (fixed.length > 0) {
-            const specialSchedule = new Set((await db.select({time: schedule.time}).from(schedule).where(eq(schedule.date, baseInt))).map(v => v.time as number));
-            for (const v of fixed)
-                if (specialSchedule.has(v.time)) await db.update(schedule).set({subject: v.subject, belongings: v.belongings ?? "", memo: v.memo ?? ""}).where(and(eq(schedule.date, baseInt), eq(schedule.time, v.time)));
-                else await db.insert(schedule).values({date: baseInt, time: v.time, subject: v.subject, belongings: v.belongings ?? "", memo: v.memo ?? "", special: 1});
-        }*/
-        //if (newSubjects.length > 0) await db.insert(subject).values(newSubjects);
+        if (add.length > 0) await db.insert(schedule).values(add.map((v: any) => ({
+            date: baseInt,
+            time: v.time,
+            subject: v.subject,
+            belongings: v.belongings,
+            memo: v.memo,
+            special: v.unique ? 2 : 1
+        })));
+        for (const up of update) await db.update(schedule).set({
+            subject: up.subject,
+            belongings: up.belongings,
+            memo: up.memo,
+            special: up.unique ? 2 : 1
+        }).where(and(eq(schedule.date, baseInt), eq(schedule.time, up.time)));
+        if (del.length > 0) await db.delete(schedule).where(and(eq(schedule.date, baseInt), inArray(schedule.time, del)));
+
+        if (newSubjects.length > 0) await db.insert(subject).values(newSubjects);
     }
 }satisfies Actions;
