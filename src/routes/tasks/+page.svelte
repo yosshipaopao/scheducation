15b21de0683ef747b48f2ac7b83ce99a29631f5d
yosshipaopao <yosshipaopao@gmail.com>
2023-08-20 13:39:11 +0900

    Update +page.svelte

--- src/routes/tasks/+page.svelte
+++ src/routes/tasks/+page.svelte
@@ -1,15 +1,30 @@
 <script lang="ts">
-    import { AccordionItem, Accordion } from 'flowbite-svelte';
+    import {AccordionItem, Accordion, Card} from 'flowbite-svelte';
+    import {ClipboardCheckSolid} from "flowbite-svelte-icons";
+
+    const unsubmitted=["a"];
 </script>
 <p class="text-9xl dark:text-white">Tasks</p>
 
 <Accordion>
     <AccordionItem>
-        <span slot="header">My Header 1</span>
-        <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint explicabo ...</p>
-        <p class="text-gray-500 dark:text-gray-400">
-            Check out this guide to learn how to <a href="/" target="_blank" rel="noreferrer" class="text-blue-600 dark:text-blue-500 hover:underline"> get started </a>
-            and start developing websites even faster with components on top of Tailwind CSS.
-        </p>
+        <span slot="header">未提出</span>
+        {#each unsubmitted as v}
+            <Card size=xl horizontal class="w-full !max-w-full">
+                <ClipboardCheckSolid/>
+            </Card>
+        {/each}
+    </AccordionItem>
+    <AccordionItem>
+        <span slot="header">期限切れ</span>
+    </AccordionItem>
+    <AccordionItem>
+        <span slot="header">今週</span>
+    </AccordionItem>
+    <AccordionItem>
+        <span slot="header">今月</span>
+    </AccordionItem>
+    <AccordionItem>
+        <span slot="header">それ以降</span>
     </AccordionItem>
 </Accordion>
