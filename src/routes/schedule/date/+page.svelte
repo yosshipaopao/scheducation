0f4f44d83180d294dab6e8ec6513955060ba1269
yosshipaopao <yosshipaopao@gmail.com>
2023-09-14 12:39:03 +0900

    PWA開始位置の変更

---
+++ src/routes/schedule/date/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import {browser} from "$app/environment";
+    import {goto} from "$app/navigation";
+
+    const now=new Date();
+    if(browser)goto(`/schedule/date/${now.getFullYear()}/${now.getMonth()+1}/${now.getDate()}`)
+</script>
